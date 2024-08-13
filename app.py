52ba88303204ed66ac7e5b77fe065259cc53094f
sandrinio123 <48046700+sandrinio123@users.noreply.github.com>
2024-08-13 14:34:50 +0400

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas
 
+data = {
+         'Series_1':[1,2,3,4,5],
+         'Series_2':[10,30,60,4000,500]
+}
+df = pandas.DataFrame(data)
 st.title("salamii")
 st.write("""gamarjoba tamunnaaa      
          """)
+
+st.write(df)
