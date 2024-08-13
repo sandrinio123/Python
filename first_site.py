b6aa3ff316c5b53f9d0e3d192cf33f0937f8dd0e
sandrinio123 <48046700+sandrinio123@users.noreply.github.com>
2024-08-13 14:17:12 +0400

    Add files via upload

---
+++ first_site.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("My First Streamlit APP")
+st.subheader("Im Writing My First Streamlit APP rn")
+st.write("""I really love this course,
+         I'm Sandro Todua,
+         remember the name       
+         """)
